ca4e58f063ad143d6d5f09b6caac740e568c83c6
BlaKe00786 <sameershaikh403@gmail.com>
2020-10-27 09:08:36 +0530

    [Sameer]Add: Displays the occurences of words in a string.

--- HashTableProject/Program.cs
+++ HashTableProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HashTableProject
 {
@@ -6,7 +7,22 @@ namespace HashTableProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyMapNode<string, int> occurrences = new MyMapNode<string, int>(1);
+            string input = "to be or not to be";
+            string[] words = input.Split(null);
+            foreach (String word in words)
+            {
+                int oldCount = occurrences.Get(word);
+                if(oldCount!=0)
+                {
+                    occurrences.Remove(word);
+                }
+                occurrences.Add(word, oldCount + 1);
+            }
+            foreach (String word in words)
+            {
+                Console.WriteLine(word + " : " + occurrences.Get(word));
+            }
         }
     }
 }
